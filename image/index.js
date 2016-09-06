5fd910877c6af532a31e508df74f3a290acec512
Andrew Kreps <andrew.kreps@gmail.com>
2016-09-05 22:00:57 -0700

    Created image lambda

--- image/index.js
+++ image/index.js
@@ -1,31 +1,23 @@
 const querystring = require('querystring');
-var google = require('googleapis');
-var customsearch = google.customsearch('v1');
-
 var GoogleSearch = require('google-search');
 var googleSearch = new GoogleSearch({
     key: 'AIzaSyAlTbxqcZOlb3M-QXR4PCYpS2U1rfgwSlU',
     cx: '016450909327860943906:3a3e35xbkzu'
 });
 
-
 exports.handler = function(event, context) {
-    //Echo back the text the user typed in
-    // context.succeed('You sent: ' + event.text
     var query = querystring.parse(event['body-json']);
     console.log(query);
     console.log(context);
     googleSearch.build({
         q: query.text,
-        num: 10 // Number of search results to return between 1 and 10, inclusive
+        searchType: 'image',
+        num: 10
     }, function(error, response) {
         console.log(response);
         success = {
             response_type: 'in_channel',
-            text: response.items[0].link,
-            attachments: [{
-                text: response.items[0].title + ': ' + response.items[0].snippet
-            }]
+            text: response.items[0].link
         };
         console.log(success);
         context.succeed(success);
