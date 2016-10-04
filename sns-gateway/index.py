3aa8edc72dbb71a422854d5e6e882bf8a5f1d1e5
Andrew Kreps <andrew.kreps@gmail.com>
2016-10-03 22:32:48 -0700

    Rebuilt the snsGateway in python for teh speeds.

---
+++ sns-gateway/index.py
@@ -0,0 +1,21 @@
+import json
+import boto3
+
+def handler(event, context):
+    print('Starting process')
+    message = event['queryStringParameters']
+    print(message)
+    client = boto3.client('sns')
+    response = client.publish(
+        TargetArn = 'arn:aws:sns:us-west-2:268578439486:slackGiphySns',
+        Message   = json.dumps({'default': json.dumps(message)}),
+        MessageStructure = 'json'
+    )
+    print(response)
+    return {
+        'statusCode': '200',
+        'body': '',
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
