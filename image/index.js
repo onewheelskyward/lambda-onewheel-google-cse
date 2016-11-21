5de0fc6e81b5f44e2ea6dc7d2a82a05c0afbdca1
Andrew Kreps <andrew.kreps@gmail.com>
2016-11-21 00:28:33 -0800

    Easter egg central

--- image/index.js
+++ image/index.js
@@ -12,23 +12,18 @@ exports.handler = function(event, context) {
     console.log(event.queryStringParameters);
     console.log("Keys: " + process.env.CSE_KEY + ", " + process.env.CSE_CX);
 
-    googleSearch.build({
-        q: event.queryStringParameters.text,
-        searchType: 'image',
-        num: 10
-    }, function(error, response) {
-        console.log(response);
+    if (event.queryStringParameters.user_name == 'adam_the_okay') {
+        var images = ['https://i.ytimg.com/vi/ZXsQAXx_ao0/maxresdefault.jpg', 'https://i.ytimg.com/vi/Alt0SKEL84M/maxresdefault.jpg', 'https://i.ytimg.com/vi/6AnAhdN9nDg/maxresdefault.jpg', 'https://i.ytimg.com/vi/Z6gG3tKDBlk/maxresdefault.jpg', 'http://cdn0.techly.com.au/wp-content/uploads/2015/06/shia.jpg', 'http://cdn.wallpapersafari.com/46/91/DumWsy.jpg', 'https://metrouk2.files.wordpress.com/2015/06/shia-labeouf.png', 'https://media.giphy.com/media/wErJXg1tIgHXG/giphy.gif', 'https://media3.giphy.com/media/esZGs2BiaIJcA/giphy.gif'];
+        var image = images[Math.floor(Math.random() * images.length)];
         success = {
             response_type: 'in_channel',
-            text: response.items[0].link
+            text: image
         };
-        console.log(success);
-        console.log("Posting to " + event.queryStringParameters.response_url);
         superagent
             .post(event.queryStringParameters.response_url)
             .send(success)
             .set('Content-type', 'application/json')
-            .end(function(err, res) {
+            .end(function (err, res) {
                 console.log("Posted successfully!");
                 context.succeed({
                     statusCode: 200,
@@ -36,5 +31,32 @@ exports.handler = function(event, context) {
                     body: ''
                 });
             });
-    });
+    } else {
+
+        googleSearch.build({
+            q: event.queryStringParameters.text,
+            searchType: 'image',
+            num: 10
+        }, function (error, response) {
+            console.log(response);
+            success = {
+                response_type: 'in_channel',
+                text: response.items[0].link
+            };
+            console.log(success);
+            console.log("Posting to " + event.queryStringParameters.response_url);
+            superagent
+                .post(event.queryStringParameters.response_url)
+                .send(success)
+                .set('Content-type', 'application/json')
+                .end(function (err, res) {
+                    console.log("Posted successfully!");
+                    context.succeed({
+                        statusCode: 200,
+                        headers: {},
+                        body: ''
+                    });
+                });
+        });
+    }
 };
