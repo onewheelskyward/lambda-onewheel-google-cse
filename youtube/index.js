10e7a7250bf8efb667ca88f5f7cdee5594b0ef3b
Andrew Kreps <andrew.kreps@gmail.com>
2016-12-10 10:07:02 -0800

    youtube added

---
+++ youtube/index.js
@@ -0,0 +1,41 @@
+var superagent = require('superagent');
+var GoogleSearch = require('google-search');
+
+var googleSearch = new GoogleSearch({
+    key: process.env.CSE_KEY,
+    cx: process.env.CSE_CX
+});
+
+exports.handler = function(event, context) {
+    // const message = JSON.parse(event.Records[0].Sns.Message);
+    // console.log('From SNS:', message);
+    // console.log(context);
+    console.log(event.queryStringParameters);
+    console.log("Keys: " + process.env.CSE_KEY + ", " + process.env.CSE_CX);
+
+    googleSearch.build({
+        q: event.queryStringParameters.text + ' site:youtube.com',
+        searchType: 'video',
+        num: 10
+    }, function (error, response) {
+        console.log(response);
+        success = {
+            response_type: 'in_channel',
+            text: response.items[0].link
+        };
+        console.log(success);
+        console.log("Posting to " + event.queryStringParameters.response_url);
+        superagent
+            .post(event.queryStringParameters.response_url)
+            .send(success)
+            .set('Content-type', 'application/json')
+            .end(function (err, res) {
+                console.log("Posted successfully!");
+                context.succeed({
+                    statusCode: 200,
+                    headers: {},
+                    body: ''
+                });
+            });
+    });
+};
